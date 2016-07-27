8d8e75844bed631a61834692118f6b28b5489d53
Athul Raj <athulbraj@gmail.com>
2016-07-27 07:37:00 +0530

    Auto

--- cv.js
+++ cv.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+
+var fs = require('fs'); // this engine requires the fs module
+app.engine('ntl', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));// this is an extremely simple template engine
+    var rendered = content.toString().replace('#title#', '<title>'+ options.title +'Athul Raj\'s CV</title>');
+    return callback(null, rendered);
+  });
+});
+app.set('views', './views'); // specify the views directory
+app.set('view engine', 'ntl'); // register the template engine
 
 app.get('/redirect', function(req, res){
 	res.redirect(302,'/');
