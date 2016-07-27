bb5af16604b99553e70276eb1cde192d551c1b93
Athul Raj <athulbraj@gmail.com>
2016-07-28 01:42:10 +0530

    Auto

--- ppdf.js
+++ ppdf.js
@@ -1,7 +1,7 @@
 var page = require('webpage').create();
 page.viewportSize = {
   width: 1366,
-  height: 6000
+  height: 768
 };
 page.open('http://127.0.0.1:9001/', function() {
   page.render('public/Athul_Raj.pdf');
