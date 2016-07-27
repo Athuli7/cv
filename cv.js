2a2a8830d486b4e1f1833823a1ab16d2079f99f2
Athul Raj <athulbraj@gmail.com>
2016-07-27 10:08:35 +0530

    Auto

--- cv.js
+++ cv.js
@@ -3,6 +3,7 @@ var app = express();
 var fs = require('fs');
 var cvContentOptions = JSON.parse(fs.readFileSync('cvContentOptions.json'));
 var cvEngineOptions = JSON.parse(fs.readFileSync('cvEngineOptions.json'));
+var package = JSON.parse(fs.readFileSync('package.json'));
 
 app.engine('cv', function (filePath, options, callback) { // define the template engine
   fs.readFile(filePath, function (err, content) {
@@ -10,16 +11,33 @@ app.engine('cv', function (filePath, options, callback) { // define the template
     //Me
     var rendered = content.toString();
     console.log(cvEngineOptions);
+    //info
     for (var cvEOin in cvEngineOptions){
-    var tempcvEO = cvEngineOptions[cvEOin];
+    	var tempcvEO = cvEngineOptions[cvEOin];
     	rendered = rendered.replace(
 	    	tempcvEO[0],
 	    	tempcvEO[1] + options[tempcvEO[2]] + tempcvEO[3]
 	    );
 	}
+	//social
+	//works
+	//xp
+	//footer
+	var depslist = "";
+	for(var depIn in package.dependencies){
+		deplist = deplist + depIn + ',';
+	}
+	deplist = deplist.substring(-1);
+	rendered = rendered.replace('#footer#',
+	"<small class=\"copyright\">Designed with <i class=\"fa fa-heart\">"+
+	"</i> on "+
+	"<a href=\"https://nodejs.org/\">Node.js</a>("+deplist+")"+
+	" by "+package.author+ " under a "+package.license+" license"+
+	"</small>");
     //!Me
     return callback(null, rendered);
-  });
+  	});
+	}
 });
 
 app.set('views', './views'); // specify the views directory
