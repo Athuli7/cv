ceb1a832041d5b4bca2b4e8a16855d7f51fb4e99
Athul Raj <athulbraj@gmail.com>
2016-07-27 08:14:17 +0530

    Auto

--- cv.js
+++ cv.js
@@ -1,19 +1,22 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var gOpts = JSON.parse(fs.readFileSync('options.json');
+var cvContentOptions = JSON.parse(fs.readFileSync('cvContentOptions.json');
+var cvEngineOptions = JSON.parse(fs.readFileSync('cvEngineOptions.json');
 
 app.engine('cv', function (filePath, options, callback) { // define the template engine
   fs.readFile(filePath, function (err, content) {
     if (err) return callback(new Error(err));// this is an extremely simple template engine
     //Me
     var rendered = content.toString();
-    rendered = rendered.replace('#title#', '<title>'+ options.title +'</title>');
-    rendered = rendered.replace('#description#','<meta name="description" content="'+options.title+'">');
-    rendered = rendered.replace('#author#', '<meta name="author" content="'+options.author+'">');
-    rendered = rendered.replace('#stylecss#', '<link id="theme-style" rel="stylesheet" href="assets/css/'+options.stylecss+'">');
+    for (var opt in cvEngineOptions){
+    //rendered = rendered.replace('#title#', '<title>'+ options.title +'</title>');
+    //rendered = rendered.replace('#description#','<meta name="description" content="'+options.title+'">');
+    //rendered = rendered.replace('#author#', '<meta name="author" content="'+options.author+'">');
+    //rendered = rendered.replace('#stylecss#', '<link id="theme-style" rel="stylesheet" href="assets/css/'+options.stylecss+'">');
     //rendered.replace('', ''+options.author+'');
-
+    	rendered = rendered.replace(opt[0],opt[1],cvContentOptions[opt[0]],opt[0]);
+	}
     //!Me
     return callback(null, rendered);
   });
